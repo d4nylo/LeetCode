7bd5228dd6fdeb42350c68dceb5966243758c77d
Danylo Kucherenko <danylo.kucherenko@gmail.com>
2022-06-01 21:58:59 +0100

    Contains Duplicate

--- LeetCode.Tests/AlgosTests.cs
+++ LeetCode.Tests/AlgosTests.cs
@@ -5,7 +5,45 @@ namespace LeetCode.Tests;
 public class AlgosTests
 {
     [Fact]
-    public void Test1()
+    public void Contains_Duplicate()
     {
+        #region Example 1
+
+        // Arrange
+        var nums1 = new int[] {1, 2, 3, 1};
+
+        // Act
+        var output1 = Algos.ContainsDuplicate(nums1);
+
+        // Assert
+        Assert.True(output1);
+
+        #endregion
+
+        #region Example 2
+
+        // Arrange
+        var nums2 = new int[] {1, 2, 3, 4};
+
+        // Act
+        var output2 = Algos.ContainsDuplicate(nums2);
+
+        // Assert
+        Assert.False(output2);
+
+        #endregion
+
+        #region Example 3
+
+        // Arrange
+        var nums3 = new int[] {1, 1, 1, 3, 3, 4, 3, 2, 4, 2};
+
+        // Act
+        var output3 = Algos.ContainsDuplicate(nums3);
+
+        // Assert
+        Assert.True(output3);
+
+        #endregion
     }
 }
--- LeetCode/Algos.cs
+++ LeetCode/Algos.cs
@@ -2,5 +2,22 @@
 
 public static class Algos
 {
-    
+    // https://leetcode.com/problems/contains-duplicate/
+    // Time Complexity: O(n) & Space Complexity: O(n)
+    public static bool ContainsDuplicate(int[] nums)
+    {
+        var hash = new HashSet<int>();
+
+        for (var idx = 0; idx < nums.Length; idx++)
+        {
+            if (hash.Contains(nums[idx]))
+            {
+                return true;
+            }
+
+            hash.Add(nums[idx]);
+        }
+
+        return false;
+    }
 }
